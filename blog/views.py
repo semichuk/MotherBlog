8db8285490b5f11cb10244951d5b3ba0f058b34a
ki <ki@ki.ki>
2022-07-02 15:56:13 +0400

    added log.log

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from urllib.request import Request
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+import logging
+
+
+
+
+logger = logging.getLogger(__name__)
+
 
 
 
 # Create your views here.
 def home(request: Request)-> HttpResponse:
+    logger.debug("hello ki!debug log ")
     print(request.META.get('REMOTE_ADDR'))
     return render(request, 'blog/homeRU.html' )
